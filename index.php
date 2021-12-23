95908114dc3b11e3239a29532a9bb5f1873c47d8
franciszhao2000 <91248800+franciszhao2000@users.noreply.github.com>
2021-12-23 13:39:41 +0100

    Index php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello tous le mondes vous êtes sur le site Force of Nature</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit explicabo debitis recusandae vel atque dolorum voluptatibus reiciendis provident tempora aliquam perspiciatis ex, tempore illo expedita quis! Laudantium nisi doloremque velit ut vel deserunt explicabo sit aspernatur voluptates ad libero perspiciatis, labore adipisci, cum dicta nesciunt facere expedita nemo magni tenetur.</p>
+    <p>Asperiores voluptates deserunt sequi odio minus nihil provident, sit, repellendus quae ullam illo repudiandae blanditiis, itaque ut consectetur cumque libero nisi laboriosam ipsum dolorem! Eaque consectetur distinctio molestias doloribus vel sequi sit nobis enim adipisci beatae commodi aspernatur possimus quaerat voluptas dolorem magnam, iure, aut placeat. Quis ipsum molestias corrupti!</p>
+</body>
+</html>
